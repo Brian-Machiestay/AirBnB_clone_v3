13c05818f8a9254fdfb1f3a0df8ceaafdeebe5f0
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-19 16:32:22 +0000

    return an api request status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+
 @app_views.route('/status')
 def status():
+    """returns the status of the request"""
     return jsonify({'status': 'OK'})
