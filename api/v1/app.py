c7ce1fc39dd27c4b1495172a7d2b009a29125e3d
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-19 16:45:24 +0000

    pep8 validate all modules and packages

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """the first version of the airbnb api"""
 from flask import Flask
-app = Flask(__name__)
-
 import os
 from models import storage
 from api.v1.views import app_views
+
+app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown(self):
     """closes the filestorage"""
     storage.close()
+
 
 if __name__ == "__main__":
     host = '0.0.0.0'
@@ -20,4 +22,4 @@ if __name__ == "__main__":
         host = os.getenv('HBNB_API_HOST')
     if os.getenv('HBNB_API_PORT'):
         port = os.getenv('HBNB_API_PORT')
-    app.run(host = host, port = port, threaded=True)
+    app.run(host=host, port=port, threaded=True)
